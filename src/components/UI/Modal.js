0ddba3bcf73a092ce7995f7f7b4f494f0207145c
Shubham27092001 <shubhampawar06474@gmail.com>
2023-10-02 16:25:27 +0530

    new branch

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import classes from "./Modal.module.css";
 import ReactDom from "react-dom";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}onClick={props.onClose}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 
 const ModalOverlay = (props) => {
@@ -14,11 +14,14 @@ const ModalOverlay = (props) => {
   );
 };
 const portalElement = document.getElementById("overlays");
-
+//ASDFKGIU
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDom.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
+      {ReactDom.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
       {ReactDom.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
